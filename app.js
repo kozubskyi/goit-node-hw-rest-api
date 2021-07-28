2b39f15ef077e46ab028c904a92105b1a3b87157
Denys Kozubskyi <denys.kozubskyi@gmail.com>
2021-07-28 14:39:21 +0300

    добавил раздачу статических файлов

--- app.js
+++ app.js
@@ -35,6 +35,7 @@ app.use(express.json())
 
 app.use("/api/contacts", contactsRouter)
 app.use("/users", usersRouter)
+app.use("/avatars", express.static("public/avatars"))
 
 app.use((req, res) => {
   res.status(404).json({ message: "Not found" })
