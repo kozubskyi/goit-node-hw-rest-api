e64ab3bc4f2584d35a38bb5c41cdc7fe5e2fc3c4
Denys Kozubskyi <denys.kozubskyi@gmail.com>
2021-07-28 14:42:04 +0300

    добавил путь редактирования аватара

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,12 +1,15 @@
 const router = require("express").Router()
-const Joi = require("joi")
-Joi.objectId = require("joi-objectid")(Joi)
+const multer = require("multer")
+const uniqid = require("uniqid")
+const path = require("path")
+const Jimp = require("jimp")
+const fs = require("fs")
 
 const { authService } = require("../../services/auth.service")
 const { validate } = require("../../helpers/validate")
 const { catchWrapper } = require("../../helpers/catch-wrapper")
 const { authorize } = require("../../helpers/authorize")
-const { userSchema } = require("../../schemas/user.schema")
+const { userSchema } = require("../../joi-schemas/user.joi-schema")
 
 router.post(
   "/signup",
@@ -49,5 +52,41 @@ router.post(
     res.status(204).send()
   })
 )
+
+const TMP_FILES_DIR_NAME = "tmp"
+const FILES_DIR_NAME = "public/avatars"
+
+const upload = multer({
+  storage: multer.diskStorage({
+    destination: TMP_FILES_DIR_NAME,
+    filename: (req, file, cb) => {
+      const filename = uniqid() + path.extname(file.originalname)
+      cb(null, filename)
+    },
+  }),
+})
+
+router.patch(
+  "/avatars",
+  authorize,
+  upload.single("avatar"),
+  compressImage,
+  catchWrapper(async (req, res, next) => {
+    const avatarURL = await authService.updateAvatar(req)
+
+    res.send({ avatarURL })
+  })
+)
+
+async function compressImage(req, res, next) {
+  const file = await Jimp.read(req.file.path)
+  const filePath = req.file.path.replace(TMP_FILES_DIR_NAME, FILES_DIR_NAME)
+  await file.resize(250, 250).quality(70).writeAsync(filePath)
+  await fs.promises.unlink(req.file.path)
+  req.file.destination = req.file.destination.replace(TMP_FILES_DIR_NAME, FILES_DIR_NAME)
+  req.path = filePath
+
+  next()
+}
 
 module.exports = router
