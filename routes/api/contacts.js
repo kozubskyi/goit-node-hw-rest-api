3c290c6da58ad0f29d4c7df4d0e916fe02f5f059
Denys Kozubskyi <denys.kozubskyi@gmail.com>
2021-07-30 00:12:06 +0300

    перенес валидацию в отдельный файл

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,71 +1,19 @@
 const router = require("express").Router()
-const Joi = require("joi")
-Joi.objectId = require("joi-objectid")(Joi)
 
+const {
+  validatePostContactBody,
+  validatePatchContactBody,
+  validateIdParams,
+  validatePatchFavoriteBody,
+} = require("../../helpers/contacts-validation")
 const { ContactModel } = require("../../model/contact.model")
 const { catchWrapper } = require("../../helpers/catch-wrapper")
 
 const notFoundObj = { message: "Not found" }
 
-// Validation
-
-function validatePostContactBody(req, res, next) {
-  const schema = Joi.object({
-    name: Joi.string().required(),
-    email: Joi.string().email().required(),
-    phone: Joi.string().required(),
-  })
-
-  const { error } = schema.validate(req.body)
-
-  if (error) return res.status(400).send({ message: `missing required ${error.details[0]?.context?.label} field` })
-
-  next()
-}
-
-function validatePatchContactBody(req, res, next) {
-  const schema = Joi.object({
-    name: Joi.string(),
-    email: Joi.string().email(),
-    phone: Joi.string(),
-  }).or("name", "email", "phone")
-
-  const { error } = schema.validate(req.body)
-
-  if (error) return res.status(400).send({ message: "missing fields" })
-
-  next()
-}
-
-function validateIdParams(req, res, next) {
-  const schema = Joi.object({
-    contactId: Joi.objectId(),
-  })
-
-  const { error } = schema.validate(req.params)
-
-  if (error) {
-    return res.status(400).send({ message: `Bad Request. Contact with id ${error._original.contactId} nod found` })
-  }
-
-  next()
-}
-
-function validatePatchFavoriteBody(req, res, next) {
-  const schema = Joi.object({
-    favorite: Joi.boolean().required(),
-  })
-
-  const { error } = schema.validate(req.body)
-
-  if (error) return res.status(400).send({ message: "missing field favorite" })
-
-  next()
-}
-
-// Methods
-
-router.get("/", async (req, res, next) => res.json(await ContactModel.find()))
+router.get("/", async (req, res, next) => {
+  res.json(await ContactModel.find())
+})
 
 router.get(
   "/:contactId",
@@ -79,7 +27,9 @@ router.get(
 router.post(
   "/",
   validatePostContactBody,
-  catchWrapper(async (req, res, next) => res.status(201).json(await ContactModel.create(req.body)))
+  catchWrapper(async (req, res, next) => {
+    res.status(201).json(await ContactModel.create(req.body))
+  })
 )
 
 router.delete(
